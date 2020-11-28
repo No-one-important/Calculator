6e0bffd5784f9b641e25acdffabd66c47f0882b6
No-one-important <72458263+No-one-important@users.noreply.github.com>
2020-11-28 11:12:57 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+    	final Scanner in = new Scanner(System.in);
+
+    	System.out.print("Enter a calculation: ");
+
+        String calc = in.nextLine();
+
+        String[] calcSplit  = calc.split(" ");
+
+        float num1 = Float.parseFloat(calcSplit[0]);
+        float num2 = Float.parseFloat(calcSplit[2]);
+        String operator = calcSplit[1];
+        float ans = 0.0F;
+        int out = 0;
+        boolean useInt = false;
+
+        switch (operator) {
+            case ("+"):
+                ans = (num1 + num2);
+            break;
+                                    
+            case ("-"):
+                ans = (num1 - num2);
+            break;
+                                    
+            case ("/"):
+                ans = (num1 / num2);
+            break;
+                                    
+            case ("*"):
+                ans = (num1 * num2);
+            break;
+                                    
+            default:
+                ans = (num1 + num2);
+            break;
+        }
+
+        if (calcSplit.length == 5) {
+            float num3 = Float.parseFloat(calcSplit[4]);
+            switch (calcSplit[3]) {
+                 case ("+"):
+                     ans = (ans + num3);
+                 break;
+  
+                 case ("-"):
+                     ans = (ans - num3);
+                 break;
+                                        
+                 case ("/"):
+                     ans = (ans / num3);
+                 break;
+                                         
+                 case ("*"):
+                     ans = (ans * num3);
+                 break;
+                                         
+                 default:
+                     ans = (ans + num3);
+                 break;
+	    }
+        }
+
+        if ((ans - Math.floor(ans)) == 0) {
+            out = (int)(Math.floor(ans));
+            useInt = true;
+        }
+
+        if (useInt)
+            System.out.println(out);
+        else
+            System.out.println(ans);
+
+    }
+}
